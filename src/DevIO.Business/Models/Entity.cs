7dd4a33b699b7fabb6c35968492b8a881349c98f
Marcos Reis <marcossummers@gmail.com>
2019-09-30 16:55:47 -0300

    Aula Mapeando as entidades para o banco de dados

---
+++ src/DevIO.Business/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevIO.Business.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
