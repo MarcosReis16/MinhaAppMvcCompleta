ac66de1e4de6b4ebb59aba8c8fb1e74ed62b7e99
Marcos Reis <marcossummers@gmail.com>
2019-10-01 12:02:17 -0300

    Aula: Mapeando as entidades em ViewModels com Automapper

--- src/DevIO.App/Startup.cs
+++ src/DevIO.App/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.DependencyInjection;
 using DevIO.Data.Context;
 using DevIO.Business.Interfaces;
 using DevIO.Data.Repositories;
+using AutoMapper;
 
 namespace DevIO.App
 {
@@ -52,6 +53,9 @@ namespace DevIO.App
             services.AddScoped<IProdutoRepository, ProdutoRepository>();
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
             services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+            //Configurar o Auto Mapper
+            services.AddAutoMapper(typeof(Startup));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
